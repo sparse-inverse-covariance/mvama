379d2c4f057485135d7c435f7fafeed38889f299
dun280 <rob.dunne@csiro.au>
2015-05-28 08:58:45 +1000

    changed to find.cluster -- removed '-1'

--- R/find.clusters.R
+++ R/find.clusters.R
@@ -1,19 +1,18 @@
 find.clusters <-
-function(g,nodes )
+    function(g,nodes )
 {
-if(is.character(nodes)){
-gn<-V(g)$label
-nodes<-match(nodes,gn)
+    if(is.character(nodes)){
+        gn<-V(g)$label
+        nodes<-match(nodes,gn)
+    }
+    if(any(is.na(nodes)))return("Error: invalid nodes")
+    # find clusters
+    f<-subgraph(g,nodes)  # vertex numbering now starts from 1 in igraph
+    res<-clusters(f)
+    #create subgraphs of each cluster
+    clust<-list()
+    for(i in 1:res$no){
+        clust[[i]]<-subgraph(f,(which(res[[1]]==i-1)))
+    }
+    list(gs=f,clust.list=clust,csize=res$csize,cno=res$no)
 }
-if(any(is.na(nodes)))return("Error: invalid nodes")
-# find clusters
-f<-subgraph(g,nodes)  # vertex numbering now starts from 1 in igraph
-res<-clusters(f)
-#create subgraphs of each cluster
-clust<-list()
-for(i in 1:res$no){
-clust[[i]]<-subgraph(f,(which(res[[1]]==i-1)))
-}
-list(gs=f,clust.list=clust,csize=res$csize,cno=res$no)
-}
-
